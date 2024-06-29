a44962fcad2f85ff814526ca3b977ff2e4d31b1e
Amim Knabben <amim.knabben@gmail.com>
2024-06-29 20:16:45 -0300

    Fixing creation of cr

--- magefiles/demo3.go
+++ magefiles/demo3.go
@@ -4,6 +4,7 @@ import (
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 	"os"
+	"time"
 )
 
 const (
@@ -19,5 +20,17 @@ func (Demo3) StartController() error {
 	os.Chdir(PATH_3)
 	sh.RunV("kubebuilder", "init", "--domain", "corp.beer", "--repo", "github.com/knabben/showcase")
 	sh.RunV("kubebuilder", "create", "api", "--group", "showcase", "--version", "v1", "--kind", "Presentation")
+	return nil
+}
+
+// Run the controller locally
+func (Demo3) Run() error {
+	os.Chdir(PATH_3)
+	sh.RunV("make", "install")
+	go func() {
+		time.Sleep(10 * time.Second)
+		sh.RunV("kubectl", "apply", "-f", "config/samples/showcase_v1_presentation.yaml")
+	}()
+	sh.RunV("make", "run")
 	return nil
 }
